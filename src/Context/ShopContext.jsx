f4da629f108fe51c0c4d4c6fadb816032e8195d9
Superhero23ufo <Superhero23ufo@github.com>
2024-04-04 19:03:34 -0700

    done with the cart

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,12 +1,32 @@
-import React, {createContext} from "react";
+import React, {createContext, useState} from "react";
 import all_product from "../Components/Asset/all_product"
 export const ShppContext = createContext(null);
 
-const ShopContextProvider = (props) =>{
-    
-    
-    const contextValue  = {all_product}
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < all_product.length+1; index++){
+        cart[index]= 0
+    }
 
+    return cart;
+}
+
+const ShopContextProvider = (props) =>{
+    const [cartItems,setCartItems] = useState(getDefaultCart())
+    
+   
+
+    const addtoCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
+    }
+    
+    const removefromCart = (itemId)=>{
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
+    
+   
+    const contextValue  = {all_product,cartItems,addtoCart,removefromCart};
     return (
 
         <ShppContext.Provider value={contextValue}>
