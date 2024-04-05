71083f3272ff6aa9a4e736ab12aca7f82efc6bd4
Superhero23ufo <Superhero23ufo@github.com>
2024-04-04 20:15:43 -0700

    almost done

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -20,19 +20,35 @@ const CartItems = () =>{
                 if(cartItems [e.id] > 0)
                 {
                     return   <div>
-                    <div className="cartitems-format">
+                    <div className="cartitems-format cartitems-format-main">
                         <img src= {e.image} alt=""  className="carticon-product-icon"/>
                         <p>{e.name}</p>
-                        <p>{e.new_price}</p>
+                        <p>${e.new_price}</p>
                         <button className="cartitems-quantity">{cartItems[e.id]}</button>
-                        <p>{e.new_price*cartItems[e.id]}</p>
+                        <p>${e.new_price*cartItems[e.id]}</p>
                         <img src={remove_icon} onClick={()=>{removefromCart(e.id)}} alt="" />
                     </div>
                     <hr />
                     </div>
                 }
+                return null;
             })}
-            
+            <div className="cartitems-down">
+                <div className="cartitems-total">
+                    <h1>cart Totals</h1>
+                    <div>
+                        <div className="cartitems-total-item">
+                            <p>Subtotal</p>
+                            <p>${0}</p>
+                        </div>
+                        <hr />
+                        <div className="cartitems-total-item">
+                            <p>Shipping Fee</p>
+                            <p>Free</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
